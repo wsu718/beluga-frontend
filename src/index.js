f8f7b4a7540a00cbbf8ea1b1b2c3e68abd5454e2
William Sulinski <w@williamsulinski.com>
2020-06-08 12:03:59 -0700

    Add TailwindCSS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './tailwind.generated.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';

1e3d350bb0677bb239454add81c597f751b92369
William Sulinski <w@williamsulinski.com>
2020-06-08 13:01:25 -0700

    Add React Router and Landing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <p>This is Beluga.</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Landing />
+        </Route>
+
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <p>This is Beluga.</p>
+        </div>
+    )
+};
+
+export default Landing;

d2dd60712b001a0ac3bd9f6962f84627449ae8e8
William Sulinski <w@williamsulinski.com>
2020-06-09 16:54:44 -0700

    Add tailwindcssui to postcss

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 module.exports = {
     plugins: [
         require('tailwindcss'),
+        require('@tailwindcss/ui'),
         require('autoprefixer'),
         ...process.env.NODE_ENV === 'production'
             ? [purgecss]

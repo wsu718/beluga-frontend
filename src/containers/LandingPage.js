6d23fd3b2fd0feba76d4d29f62b30578f123a9ef
William Sulinski <w@williamsulinski.com>
2020-06-12 18:27:38 -0700

    Edit LandingPage

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // import Logo from '../images/logo.svg';
 // import Whale1 from '../images/whale-1.svg'
 // import Whale2 from '../images/whale-2.svg'
 // import Whale3 from '../images/whale-3.svg'
-// import Whale4 from '../images/whale-4.svg'
-import Whale5 from '../images/whale-5.svg'
+import Whale4 from '../images/whale-4.svg'
+// import Whale5 from '../images/whale-5.svg'
 
 
 const LandingPage = () => {
@@ -27,9 +27,9 @@ const LandingPage = () => {
                         <div className="flex items-center justify-between w-full md:w-auto md:mx-auto">
 
                             {/* Logo */}
-                            <NavLink to='/' className="md:mx-auto">
-                                <img className="h-10 w-auto sm:h-10 md:mx-auto" src={Whale5} alt="Beluga" />
-                            </NavLink>
+                            <p className="md:mx-auto">
+                                <img className="h-14 w-auto sm:h-14 md:mx-auto" src={Whale4} alt="Beluga" />
+                            </p>
 
                         </div>
 
@@ -45,29 +45,29 @@ const LandingPage = () => {
 
                         <div className="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
                             Coming soon
-                            </div>
+                        </div>
 
-                        <h2 className="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
+                        <h2 className="my-4 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
                             Beluga: a place for
-                                    <br />
-
+                            <br />
                             <span className="text-indigo-600">
                                 thoughtful discourse.
-                                </span>
+                            </span>
                         </h2>
 
                         {/* Subtext */}
-                        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                            Our relationships are what make us who we are. But too often, technology amplifies the worst parts of us. We want to fix online conversation.</p>
+                        <p className="mt-6 text-base text-gray-500 sm:mt-10 sm:text-xl lg:text-lg xl:text-xl">
+                            Our relationships are what make us who we are. But too often, technology amplifies the worst parts of us. We want to fix online conversation.
+                        </p>
 
-                        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Beluga is a way to connect with smart, supportive people, to learn new things, and share wisdom. It is a place that connects us, and bring us closer together.
-                            </p>
+                        <p className="mt-6 text-base text-gray-500 sm:mt-10 sm:text-xl lg:text-lg xl:text-xl">Beluga is a way to connect with smart, supportive people, to learn new things, and share wisdom. It is a place that connects us, and bring us closer together.
+                        </p>
 
                         {/* Email capture box */}
-                        <div className="mt-5 sm:max-w-lg sm:mx-auto sm:text-center">
+                        <div className="mt-5 sm:max-w-lg sm:mx-auto sm:text-center sm:mt-10">
                             <p className="text-base font-medium text-gray-900">
                                 We'll let you know when we're ready!
-                                </p>
+                            </p>
 
                             <form onSubmit={handleSubmit} className="mt-3 sm:flex">
                                 <input aria-label="Email" className="appearance-none block w-full px-3 py-3 border border-gray-300 text-base leading-6 rounded-md placeholder-gray-500 shadow-sm focus:outline-none focus:placeholder-gray-400 focus:shadow-outline focus:border-blue-300 transition duration-150 ease-in-out sm:flex-1" placeholder="Enter your email" />
@@ -79,15 +79,15 @@ const LandingPage = () => {
 
                             <p className="mt-3 text-sm leading-5 text-gray-500">
                                 We care about the protection of your data. Read our
-                                        <Link to="/" className="font-medium text-gray-900 underline ml-1">
+                                    <Link to="/" className="font-medium text-gray-900 underline ml-1">
                                     Privacy Policy
-                                        </Link>.
+                                    </Link>.
                             </p>
                         </div>
                     </div>
                 </div>
-            </div >
-        </div >
+            </div>
+        </div>
     )
 };
 

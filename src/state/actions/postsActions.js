baa562885f237f64af9530edf3c03965899bebbf
spencer-mcguire <smcguire1826@gmail.com>
2020-06-19 12:29:17 -0500

    add query to delete post action

--- src/state/actions/postsActions.js
+++ src/state/actions/postsActions.js
@@ -32,69 +32,59 @@ export const DELETE_POST_FAILURE = 'DELETE_POST_FAILURE';
 // export const DELETE_COMMENT_SUCCESS = 'DELETE_COMMENT_SUCCESS';
 // export const DELETE_COMMENT_FAILURE = 'DELETE_COMMENT_FAILURE';
 
-export const getPosts = () => dispatch => {
-    axios.defaults.withCredentials = true
-    dispatch({ type: GET_POSTS_START })
-    axios
-        .get(`${process.env.REACT_APP_API_URL}/api/posts`)
-        .then(res =>
-            dispatch({ type: GET_POSTS_SUCCESS, payload: res.data })
-        )
-        .catch(err =>
-            dispatch({ type: GET_POSTS_FAILURE, payload: err.response })
-        )
-}
+export const getPosts = () => (dispatch) => {
+  axios.defaults.withCredentials = true;
+  dispatch({ type: GET_POSTS_START });
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/api/posts`)
+    .then((res) => dispatch({ type: GET_POSTS_SUCCESS, payload: res.data }))
+    .catch((err) =>
+      dispatch({ type: GET_POSTS_FAILURE, payload: err.response })
+    );
+};
 
-export const getPostByID = id => dispatch => {
-    axios.defaults.withCredentials = true
-    dispatch({ type: GET_POST_BY_ID_START })
-    axios
-        .get(`${process.env.REACT_APP_API_URL}/api/posts?post=${id}`)
-        .then(res =>
-            dispatch({ type: GET_POST_BY_ID_SUCCESS, payload: res.data })
-        )
-        .catch(err =>
-            dispatch({ type: GET_POST_BY_ID_FAILURE, payload: err.response })
-        )
-}
+export const getPostByID = (id) => (dispatch) => {
+  axios.defaults.withCredentials = true;
+  dispatch({ type: GET_POST_BY_ID_START });
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/api/posts?post=${id}`)
+    .then((res) =>
+      dispatch({ type: GET_POST_BY_ID_SUCCESS, payload: res.data })
+    )
+    .catch((err) =>
+      dispatch({ type: GET_POST_BY_ID_FAILURE, payload: err.response })
+    );
+};
 
-export const addPost = data => dispatch => {
-    axios.defaults.withCredentials = true
-    dispatch({ type: ADD_POST_START })
-    axios
-        .post(`${process.env.REACT_APP_API_URL}/api/posts`, data
-        )
-        .then(res =>
-            dispatch({ type: ADD_POST_SUCCESS, payload: res.data })
-        )
-        .catch(err =>
-            dispatch({ type: ADD_POST_FAILURE, payload: err.response })
-        )
-}
+export const addPost = (data) => (dispatch) => {
+  axios.defaults.withCredentials = true;
+  dispatch({ type: ADD_POST_START });
+  axios
+    .post(`${process.env.REACT_APP_API_URL}/api/posts`, data)
+    .then((res) => dispatch({ type: ADD_POST_SUCCESS, payload: res.data }))
+    .catch((err) =>
+      dispatch({ type: ADD_POST_FAILURE, payload: err.response })
+    );
+};
 
-export const editPost = data => dispatch => {
-    axios.defaults.withCredentials = true
-    dispatch({ type: EDIT_POST_START })
-    axios
-        .put(`${process.env.REACT_APP_API_URL}/api/posts`, data)
-        .then(res =>
-            dispatch({ type: EDIT_POST_SUCCESS, payload: res.data })
-        )
-        .catch(err =>
-            dispatch({ type: EDIT_POST_FAILURE, payload: err.response })
-        )
-}
+export const editPost = (data) => (dispatch) => {
+  axios.defaults.withCredentials = true;
+  dispatch({ type: EDIT_POST_START });
+  axios
+    .put(`${process.env.REACT_APP_API_URL}/api/posts`, data)
+    .then((res) => dispatch({ type: EDIT_POST_SUCCESS, payload: res.data }))
+    .catch((err) =>
+      dispatch({ type: EDIT_POST_FAILURE, payload: err.response })
+    );
+};
 
-export const deletePost = id => dispatch => {
-    axios.defaults.withCredentials = true
-    dispatch({ type: DELETE_POST_START })
-    axios
-        .delete(`${process.env.REACT_APP_API_URL}/api/posts`, id)
-        .then(res =>
-            dispatch({ type: DELETE_POST_SUCCESS, payload: res.data })
-        )
-        .catch(err =>
-            dispatch({ type: DELETE_POST_FAILURE, payload: err.response })
-        )
-}
-
+export const deletePost = (id) => (dispatch) => {
+  axios.defaults.withCredentials = true;
+  dispatch({ type: DELETE_POST_START });
+  axios
+    .delete(`${process.env.REACT_APP_API_URL}/api/posts?post=${id}`)
+    .then((res) => dispatch({ type: DELETE_POST_SUCCESS, payload: res.data }))
+    .catch((err) =>
+      dispatch({ type: DELETE_POST_FAILURE, payload: err.response })
+    );
+};
